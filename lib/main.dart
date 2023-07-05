db948aba0ba73901c1a913cf4d0aacd85b2fb450
bora park <jemini9595@gmail.com>
2023-07-05 22:17:10 +0900

    test

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: '세준세준세준세준',
+      title: 'title',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
