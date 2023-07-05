e0d9904780dea6cc89ebf7f354e1ad13b51c2314
bora park <jemini9595@gmail.com>
2023-07-05 17:12:50 +0900

    아무거나

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'titlezzzzzzzzzzzzz',
+      title: '아무거나 변동변동',
       theme: ThemeData(
         // This is the theme of your application.
         //
