a341fbf95702edea9ada1d86df5f4b470b5aee94
bora park <jemini9595@gmail.com>
2023-07-06 15:31:12 +0900

    dev

--- lib/main.dart
+++ lib/main.dart
@@ -5,64 +5,72 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'title2',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<HomePage> createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+    return DefaultTabController(
+      initialIndex: 1,
+      length: 4,
+      child: Builder(
+        builder: (context) {
+          DefaultTabController.of(context)?.addListener(
+            () {
+              setState(() {});
+            },
+          );
+          return Scaffold(
+            body: Stack(
+              children: [
+                TabBarView(
+                  children: FirstTab(),
+                  SecondTab(),
+                  ThirdTab(),
+                  FourthTab(),
+                ),
+                SafeArea(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 20, horizontal: 16),
+                    child: Column(
+                      children: [
+                        Container(
+                          alignment: Alignment.topCenter,
+                          child: TabPageSelector(
+                            color: DefaultTabController.of(context)?.index == 1
+                                ? Colors.black38
+                                : Colors.grey[400],
+                            selectedColor:
+                                DefaultTabController.of(context)?.index == 1
+                                    ? Colors.white
+                                    : Colors.black26,
+                            indicatorSize: 8,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.list),
+          );
+        },
       ),
     );
   }
